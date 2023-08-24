1145369417c353dc8a664ae33b6a08ab99bfdd34
Diogo Neiss <diogo.oliveiran@gmail.com>
2023-08-24 00:51:32 -0300

    melhora na responsividade das fontes

--- imports/ui/pages/SignUp/formHeader.jsx
+++ imports/ui/pages/SignUp/formHeader.jsx
@@ -7,14 +7,13 @@ import { AccountStatus, getAccountStatus } from '../../utils/accountStatus'
 
 const FormHeader = ({ accountStatus }) => (
     <Box mb={2}>
-      <Typography align='center' variant='h3' gutterBottom>Cadastro de usuário</Typography>
       {accountStatus === AccountStatus.PARTIAL &&
         <Typography align='center' variant='h4'>Preencha esses dados para completar seu cadastro</Typography>
       }
   
       {accountStatus === AccountStatus.LOGGED_OUT &&
         <>
-          <Typography align='center' variant='h3'>Crie sua conta ou agilize o processo com o Github</Typography>
+          <Typography align='center' variant='h4'>Crie sua conta ou agilize o processo com o Github</Typography>
           <LoginWithGithub />
         </>
       }
