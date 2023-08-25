5cd38240f765778879fc7fcd9c0f09b253eb96bc
Diogo Neiss <diogo.oliveiran@gmail.com>
2023-08-25 13:03:39 -0300

    favicons e problema da margem do body

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 
-import Home from '/imports/ui/Home.jsx';
+import Home from '../imports/ui/Home.jsx';
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
